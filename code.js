0f04091adbcfe756cba1d52602dc16763977880c
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-02-17 12:48:30 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
-function fib(n) {
-    return n;
+function FibExplict(n){
+
+    return Math.floor((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5))
+
+}
+
+function fib(n){
+    //Recursive closer to ensure arr does not change with each recursive call
+    function fib_(n,arr){
+        if(n<1){
+            arr.unshift(0)
+            //console.log(arr.length)
+            return arr;
+        }
+        arr.unshift(Math.floor(FibExplict(n))) // Appends each fibbonachi value to the beggining of the list
+        return fib_(n-1,arr)
+    }
+    return fib_(n,[])
+
 }
