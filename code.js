efbd080a38f4de3821ffacdeb12359ce385b105b
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-02-18 14:21:11 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,9 @@ function fib(n){
     function fib_(n,arr){
         var first=arr.length-1
         var second=arr.length-2
+        if(n==0){
+            return a=[0];
+        }
         if(n<=1){
             return arr;
         }
